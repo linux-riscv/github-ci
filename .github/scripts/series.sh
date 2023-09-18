d68f4a547bd3830102ba79e1dc30f56545334df2
Björn Töpel <bjorn@rivosinc.com>
2023-09-18 15:06:02 +0200

    Remove incorrect git-reset argument
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- .github/scripts/series.sh
+++ .github/scripts/series.sh
@@ -13,7 +13,7 @@ group_start "Series"
 tcnt=1
 tests=( $(ls ${d}/series/*.sh) )
 for i in "${tests[@]}"; do
-    git reset --hard $i
+    git reset --hard HEAD
     msg="Test ${tcnt}/${#tests[@]}: ${i}"
     echo "::group::${msg} @ $(date --utc +%Y-%m-%dT%H:%M:%S.%NZ)"
     bash ${i} "${msg}" || true
