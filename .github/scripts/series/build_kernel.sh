b192ffd7d55d5df339f920a0176ffdea947b2316
Alexandre Ghiti <alexghiti@rivosinc.com>
2024-11-13 15:57:46 +0000

    Add ltp build
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>

--- .github/scripts/series/build_kernel.sh
+++ .github/scripts/series/build_kernel.sh
@@ -68,6 +68,8 @@ elif [[ $config == "kselftest" ]]; then
 					     <(echo "CONFIG_KERNEL_UNCOMPRESSED=y")
     make_wrap olddefconfig
     keep_build=1
+elif [[ $config == "ltp" ]]; then
+    make_wrap ubuntu_defconfig
 else
     if [[ $fragment == "plain" ]]; then
         $lnxroot/scripts/kconfig/merge_config.sh -y -m -O $output $lnxroot/arch/riscv/configs/$config \
