afb003a418276cd1b296f187b59e78916cc0022a
Alexandre Ghiti <alexghiti@rivosinc.com>
2024-11-13 15:57:46 +0000

    Add ltp build
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>

---
+++ .github/scripts/ltp.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Rivos Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -euox pipefail
+d=$(dirname "${BASH_SOURCE[0]}")
+. $d/series/utils.sh
+
+logs=$(get_logs_dir)
+f=${logs}/ltp.log
+
+date -Iseconds | tee -a ${f}
+echo "Build, boot, and run ltp on various kernels" | tee -a ${f}
+echo "Top 16 commits" | tee -a ${f}
+git log -16 --abbrev=12 --pretty="commit %h (\"%s\")" | tee -a ${f}
+build_name=`git describe --tags`
+
+# Build the kernel that will run LTP
+export CI_TRIPLE="riscv64-linux-gnu"
+cp $d/series/kconfigs/ubuntu_defconfig arch/riscv/configs/
+$d/series/kernel_builder.sh rv64 ltp plain gcc
+KERNEL_PATH=$(find "/build/$(gen_kernel_name rv64 ltp plain gcc)" -name '*vmlinuz*')
+mv $KERNEL_PATH $KERNEL_PATH.gz
+gunzip $KERNEL_PATH.gz
+
+# Clone our own tuxrun
+# FIXME: did not find a way to set qemu cpu using QEMU_CPU
+cd /build
+git clone https://gitlab.com/alexghiti/tuxrun -b dev/alex/riscv64
+
+python3 -m venv .env
+source .env/bin/activate
+pip install -U tuxrun
+
+# TODO ltp-controllers is too slow for now because of cgroup_fj_stress.sh
+# but I haven't found an easy to skip this one from tuxrun
+ltp_tests=( "ltp-commands"  "ltp-syscalls" "ltp-mm" "ltp-hugetlb" "ltp-crypto" "ltp-cve" "ltp-containers" "ltp-fs" "ltp-sched" )
+
+mkdir -p /build/squad_json/
+
+for ltp_test in ${ltp_tests[@]}; do
+    ./tuxrun/run --runtime null --device qemu-riscv64 --kernel $KERNEL_PATH --tests $ltp_test --results /build/squad_json/$ltp_test.json --log-file-text /build/squad_json/$ltp_test.log --timeouts $ltp_test=480 || true
+
+    # Convert JSON to squad datamodel
+    python3 /build/my-linux/.github/scripts/series/tuxrun_to_squad_json.py --result-path /build/squad_json/$ltp_test.json
+
+    curl --header "Authorization: token $SQUAD_TOKEN" --form tests=@/build/squad_json/$ltp_test.squad.json --form log=@/build/squad_json/$ltp_test.log https://mazarinen.tail1c623.ts.net/api/submit/riscv-linux/linux-all/$build_name/qemu
+done
---
+++ .github/scripts/series/tuxrun_to_squad_json.py
@@ -0,0 +1,52 @@
+import argparse
+import json
+
+"""
+Turn
+{
+    "testsuiteA": {
+        "test1": {}
+    },
+    "testsuiteB": {
+        "test1": {}
+    }
+}
+
+into
+
+{
+    "testsuiteA/test1": {}
+    "testsuiteB/test1": {}
+}
+
+which is what is expected by Squad.
+"""
+
+def parse_args():
+    parser = argparse.ArgumentParser(description = 'Output Squad tests results for tuxrun LTP')
+    parser.add_argument("--result-path", default = "",
+            help = 'Path to the tuxrun JSON result file')
+
+    return parser.parse_args()
+
+def generate_squad_json(result_path):
+    dict_results = {}
+
+    with open(result_path, "r") as f:
+        dict_initial = json.loads(f.read())
+
+    # Search only the first dimension for keys starting with "ltp-"
+    for k, v in dict_initial.items():
+        if k.startswith("ltp-"):
+            for ltp_key, ltp_value in v.items():
+                dict_results[k + "/" + ltp_key] = ltp_value
+
+    print(dict_results)
+
+    with open(result_path.replace(".json", ".squad.json"), "w") as f:
+        json.dump(dict_results, f)
+
+if __name__ == "__main__":
+    args = parse_args()
+    generate_squad_json(args.result_path)
+
