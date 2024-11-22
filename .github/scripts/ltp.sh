fc178a5e45906bd6c56042966ec4b489fd49d864
Alexandre Ghiti <alexghiti@rivosinc.com>
2024-11-22 09:05:28 +0000

    Add metadata to series and testsuites
    This allows to have the link to the GH workflow.
    
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>

--- .github/scripts/ltp.sh
+++ .github/scripts/ltp.sh
@@ -44,6 +44,7 @@ for ltp_test in ${ltp_tests[@]}; do
 
     # Convert JSON to squad datamodel
     python3 /build/my-linux/.github/scripts/series/tuxrun_to_squad_json.py --result-path /build/squad_json/$ltp_test.json
+    python3 /build/my-linux/.github/scripts/series/generate_metadata.py --logs-path /build/squad_json/ --job-url ${GITHUB_JOB_URL}
 
-    curl --header "Authorization: token $SQUAD_TOKEN" --form tests=@/build/squad_json/$ltp_test.squad.json --form log=@/build/squad_json/$ltp_test.log https://mazarinen.tail1c623.ts.net/api/submit/riscv-linux/linux-all/$build_name/qemu
+    curl --header "Authorization: token $SQUAD_TOKEN" --form tests=@/build/squad_json/$ltp_test.squad.json --form log=@/build/squad_json/$ltp_test.log --form metadata=@/build/squad_json/metadata.json https://mazarinen.tail1c623.ts.net/api/submit/riscv-linux/linux-all/$build_name/qemu
 done
