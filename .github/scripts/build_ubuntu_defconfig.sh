5d93e4b5e82aca60312b194955e9f4b9da283a68
Alexandre Ghiti <alexghiti@rivosinc.com>
2025-06-17 09:54:55 +0000

    Use 13 characters revision to match kselftests
    So that we don't end up with 2 different entries in squad.
    
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>

--- .github/scripts/build_ubuntu_defconfig.sh
+++ .github/scripts/build_ubuntu_defconfig.sh
@@ -16,8 +16,8 @@ echo "Top 16 commits" | tee -a ${f}
 git log -16 --abbrev=12 --pretty="commit %h (\"%s\")" | tee -a ${f}
 
 kernel_base_sha=$(git log -1 --pretty=%H $(git log -1 --reverse --pretty=%H .github)^)
-echo "build_name $(git describe --tags ${kernel_base_sha})" | tee -a ${f}
-build_name=$(git describe --tags ${kernel_base_sha})
+build_name=$(git describe --tags --abbrev=13 ${kernel_base_sha})
+echo "build_name ${build_name}" | tee -a ${f}
 
 # Build the kernel that will run LTP
 export CI_TRIPLE="riscv64-unknown-linux-gnu"
